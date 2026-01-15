ac47b92a87617fe4b9a09e4e1060a4a456a621cb
solssol7 <sol@aswemake.com>
2026-01-15 19:29:30 +0900

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,8 @@
 // assets/js/config.js
 window.APP_CONFIG = {
-  QUIZ_VERSION: "v1-modular-centered",
+  QUIZ_VERSION: "v3",
   // Supabase 연동 전이면 빈 문자열로 둬도 됨(이벤트 로깅은 스킵 처리됨)
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://htcxkbijmiptoubmkhkm.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0Y3hrYmlqbWlwdG91Ym1raGttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0MjA2OTcsImV4cCI6MjA3ODk5NjY5N30.JGTUpL5spuYEvXdI3u3f3qboS6n2ztGSVwHrZijB4i8",
 };
+
